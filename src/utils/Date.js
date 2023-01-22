8f1bf806afc10595c3bd21a60241c7a86b759164
Lucas Almeida <lukasalmeida372@gmail.com>
2023-01-22 20:56:46 -0300

    corrections in date system

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -8,3 +8,15 @@ export function getDateDdMm() {
 
 	return dd + "/" + mm;
 }
+
+export function getDateDdMmYy() {
+	const today = new Date();
+	let mm = today.getMonth() + 1; // Months start at 0!
+	let dd = today.getDate();
+	let yy = today.getFullYear();
+
+	if (dd < 10) dd = "0" + dd;
+	if (mm < 10) mm = "0" + mm;
+
+	return dd + "/" + mm + "/" + yy;
+}
